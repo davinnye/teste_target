c249a942b4fe503c168f66a88e248aa1bcd96326
Vinicius 'Davina' Ferreira <63323842+davinnye@users.noreply.github.com>
2024-09-23 10:50:28 -0300

    questão 3 resolvida

---
+++ questao_3.java
@@ -0,0 +1,12 @@
+public class questao_3 {
+    public static void main(String[] args) {
+        int indice = 12;
+        long soma = 0;
+
+        for (int k = 1; k < indice; k++) {
+            soma = soma + k;
+        }
+        System.out.println("O valor final da variável SOMA será de: " + soma);
+        //o valor total é de 66
+    }
+}
